1309418dd71dc8ce0e5df5d447d0c408a692ef29
Bachman Rocks <azfaryar004@gmail.com>
2020-12-11 20:52:47 +0500

    Homepage

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ejs = require("ejs");
 const path = require("path");
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
