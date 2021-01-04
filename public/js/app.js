c720ffce842927f72c747c319a2c1549ae5b7d2d
Bachman Rocks <azfaryar004@gmail.com>
2021-01-04 18:17:55 +0500

    add to cart + login reg

--- public/js/app.js
+++ public/js/app.js
@@ -104,7 +104,7 @@ console.log("Hi Boss");
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(59:2) Missed semicolon\n\n \u001b[90m 57 | \u001b[39m\t\u001b[33m}\u001b[39m\n \u001b[90m 58 | \u001b[39m\t&\u001b[33m:\u001b[39mhover \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 59 | \u001b[39m\t\tcolor\u001b[33m:\u001b[39m &primary-hover\u001b[33m;\u001b[39m\n \u001b[90m    | \u001b[39m\t\u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 60 | \u001b[39m\t\u001b[33m}\u001b[39m\n \u001b[90m 61 | \u001b[39m\u001b[33m}\u001b[39m\n\n    at C:\\Users\\Azfar\\Desktop\\Backend\\Project\\Amart\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\Azfar\\Desktop\\Backend\\Project\\Amart\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\Azfar\\Desktop\\Backend\\Project\\Amart\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\Azfar\\Desktop\\Backend\\Project\\Amart\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\Users\\Azfar\\Desktop\\Backend\\Project\\Amart\\node_modules\\postcss-loader\\src\\index.js:207:9");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
