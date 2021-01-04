3d4f8d83a57df33324d55934a361a0b49846ad37
Bachman Rocks <azfaryar004@gmail.com>
2021-01-04 18:05:29 +0500

    part-5

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
+var expressLayout = require("express-ejs-layouts");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const ejs = require("ejs");
 const path = require("path");
 
 app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
 	res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+	res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+	res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+	res.render("auth/register");
 });
 
 app.listen(PORT, () => {
